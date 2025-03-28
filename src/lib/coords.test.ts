fb4ccf349e342ea6f4e20d4507bde4b76b6c25e2
Paul <paulwharin@gmail.com>
2025-03-28 15:40:22 +0000

    feat: provide coords -> index converter

--- src/lib/coords.test.ts
+++ src/lib/coords.test.ts
@@ -1,4 +1,4 @@
-import { type Coords } from "./coords";
+import { convertCoords, type Coords } from "./coords";
 
 describe("coords tuple", () => {
   test("should create coords", () => {
@@ -6,5 +6,26 @@ describe("coords tuple", () => {
 
     expect(position[0]).toBe(3);
     expect(position[1]).toBe(4);
+  });
+
+  test("should return Coords converter given board dimensions", () => {
+    const dimensions: Coords = [4, 5];
+
+    const toIndex = convertCoords(dimensions);
+
+    expect(toIndex).not.toBeNull();
+  });
+});
+
+describe("convert coords to index", () => {
+  const dimensions: Coords = [4, 5];
+  const toIndex = convertCoords(dimensions);
+
+  test.each([
+    [[0, 0], 0],
+    [[0, 1], 4],
+    [[3, 4], 19],
+  ])(`should convert coords(%o) -> index: %i`, (coords: Coords, expected) => {
+    expect(toIndex(coords)).toBe(expected);
   });
 });
--- src/lib/coords.ts
+++ src/lib/coords.ts
@@ -1,3 +1,14 @@
 type Coords = [x: number, y: number];
 
+function convertCoords(dimensions: Coords) {
+  const [width] = dimensions;
+
+  return function (coords: Coords) {
+    const [x, y] = coords;
+
+    return y * width + x;
+  };
+}
+
 export type { Coords };
+export { convertCoords };
